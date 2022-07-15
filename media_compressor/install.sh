79a838e673eeb4513735344a7ead615e4798ac6a
kmou424 <me@kmou424.moe>
2022-07-15 23:59:37 +0800

    feat: Add size_calc to install shell script

--- media_compressor/install.sh
+++ media_compressor/install.sh
@@ -1,4 +1,5 @@
 bash env.sh
 SCRIPT_DIRECTORY=$(dirname $(readlink -f "$0"))
 echo 'alias media_compressor="python '"$SCRIPT_DIRECTORY"'/media_compressor.py"' >> $HOME/.bashrc
+echo 'alias size_calc="python '"$SCRIPT_DIRECTORY"'/size_calc.py"' >> $HOME/.bashrc
 source $HOME/.bashrc
