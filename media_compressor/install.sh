4320ead18d2b59eb5d66bd0baaa557d1f655b328
kmou424 <me@kmou424.moe>
2022-07-15 23:58:37 +0800

    chore: Shell more normalized

--- media_compressor/install.sh
+++ media_compressor/install.sh
@@ -1,4 +1,4 @@
 bash env.sh
 SCRIPT_DIRECTORY=$(dirname $(readlink -f "$0"))
-echo 'alias media_compressor="python' $SCRIPT_DIRECTORY'/media_compressor.py"' >> $HOME/.bashrc
+echo 'alias media_compressor="python '"$SCRIPT_DIRECTORY"'/media_compressor.py"' >> $HOME/.bashrc
 source $HOME/.bashrc
