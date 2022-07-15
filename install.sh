6feb2e484ac054dd6de665981e9c5e52e7a14543
kmou424 <me@kmou424.moe>
2022-07-16 00:13:04 +0800

    feat: Add prog_scanner into install shell script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 sudo bash env.sh
 SCRIPT_DIRECTORY=$(dirname $(readlink -f "$0"))
 echo 'alias media_compressor="python '"$SCRIPT_DIRECTORY"'/media_compressor.py"' >> $HOME/.bashrc
+echo 'alias prog_scanner="python '"$SCRIPT_DIRECTORY"'/prog_scanner.py"' >> $HOME/.bashrc
 echo 'alias size_calc="python '"$SCRIPT_DIRECTORY"'/size_calc.py"' >> $HOME/.bashrc
 source $HOME/.bashrc
